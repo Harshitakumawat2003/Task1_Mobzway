367f11076ed86a535bd7db824f63e859cdc6661f
Harshitakumawat2003 <140372177+Harshitakumawat2003@users.noreply.github.com>
2024-08-13 21:44:00 +0530

    updated commit

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ const path = require("path");
 
 const mongoURI =
   // "mongodb+srv://harshitakumawat:hkumawat26@cluster0.abiwdnd.mongodb.net/";
-  "mongodb+srv://harshitakumawat:<password>@cluster0.abiwdnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test";
+  "mongodb+srv://harshitakumawat:hkumawat26@cluster0.abiwdnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test";
 
 // Connect to MongoDB
 mongoose
